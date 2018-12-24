b4994a5cd0a5e0168ba62037a349974b079b794a
Michael Farrell <micolous+git@gmail.com>
2018-12-25 10:29:37 +1030

    Base64String: Add nullability annotations, .equals, .toString, .empty.
    Also makes data **always** non-null.

--- src/main/java/au/id/micolous/metrodroid/xml/Base64String.java
+++ src/main/java/au/id/micolous/metrodroid/xml/Base64String.java
@@ -2,6 +2,7 @@
  * Base64String.java
  *
  * Copyright (C) 2014 Eric Butler <eric@codebutler.com>
+ * Copyright 2018 Michael Farrell <micolous+git@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,25 +19,92 @@
  */
 package au.id.micolous.metrodroid.xml;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Base64;
 
+import java.util.Arrays;
+
+import au.id.micolous.metrodroid.util.Utils;
+
+/**
+ * Wrapper for the XML serialiser, that encodes <code>byte[]</code> as a
+ * Base64 string.
+ */
 public class Base64String {
     private final byte[] mData;
 
-    public Base64String(byte[] data) {
-        mData = data;
+    private static final Base64String EMPTY = new Base64String();
+
+    private Base64String() {
+        mData = new byte[0];
     }
 
-    public Base64String(String data) {
+    /**
+     * Builds a new Base64String, taking a <code>byte[]</code> as input.
+     *
+     * If {@param data} is null, then a zero-length byte array will be used.
+     *
+     * If {@param data} is known to be constantly null, use {@link #empty()}
+     * instead.
+     *
+     * @param data Data to encode
+     */
+    public Base64String(@Nullable byte[] data) {
+        if (data == null) {
+            mData = new byte[0];
+        } else {
+            mData = data;
+        }
+    }
+
+    /**
+     * Builds a new Base64String, taking a Base64 encoded string as input.
+     * @param data Base64 encoded data
+     * @throws IllegalArgumentException On invalid Base64 data.
+     */
+    public Base64String(String data) throws IllegalArgumentException {
         mData = Base64.decode(data, Base64.DEFAULT);
     }
 
+    /**
+     * Gets the underlying data in this Base64String.
+     */
+    @NonNull
     public byte[] getData() {
         return mData;
     }
 
+    @NonNull
     public String toBase64() {
         return Base64.encodeToString(mData, Base64.NO_WRAP);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (!(obj instanceof Base64String)) {
+            return false;
+        }
+
+        Base64String other = (Base64String)obj;
+        return Arrays.equals(mData, other.mData);
+    }
+
+    /**
+     * Returns this Base64 string encoded in base16.
+     */
+    @NonNull
+    @Override
+    public String toString() {
+        return Utils.getHexString(mData);
+    }
+
+    /**
+     * Returns a zero-length Base64String.
+     */
+    @NonNull
+    public static Base64String empty() {
+        return EMPTY;
     }
 
     public static final class Transform implements org.simpleframework.xml.transform.Transform<Base64String> {

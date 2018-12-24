b554a1047a58d75c92571ab74ad960da07c478a1
Michael Farrell <micolous+git@gmail.com>
2018-12-25 10:19:25 +1030

    Delete unused imports

--- src/main/java/au/id/micolous/metrodroid/fragment/CardRawDataFragment.java
+++ src/main/java/au/id/micolous/metrodroid/fragment/CardRawDataFragment.java
@@ -1,10 +1,7 @@
 package au.id.micolous.metrodroid.fragment;
 
-import org.simpleframework.xml.Serializer;
-
 import java.util.List;
 
-import au.id.micolous.metrodroid.MetrodroidApplication;
 import au.id.micolous.metrodroid.activity.AdvancedCardInfoActivity;
 import au.id.micolous.metrodroid.card.Card;
 import au.id.micolous.metrodroid.ui.ListItem;

d7f484aa9c0ab7a45b335e8d792ff9151270dd70
Vladimir Serbinenko <phcoder@gmail.com>
2021-10-04 00:16:44 +0200

    currencyNameBySymbol: Add fallback for old Android versions

--- src/jvmCommonMain/kotlin/au/id/micolous/metrodroid/util/Country.kt
+++ src/jvmCommonMain/kotlin/au/id/micolous/metrodroid/util/Country.kt
@@ -1,10 +1,17 @@
 @file:JvmName("CountryKtActual")
 package au.id.micolous.metrodroid.util
 
+import android.os.Build
 import java.util.*
 
 actual fun currencyNameBySymbol(symbol: String): String? =
-    Currency.getInstance(symbol)?.displayName
+    Currency.getInstance(symbol)?.let {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            it.displayName
+        } else {
+            it.currencyCode
+        }
+    }
 
 private val specialLocales = mapOf(
     "CA" to Locale.CANADA,

d4c924a8d9e00aa9d717bb34087f25f9cab71726
Vladimir Serbinenko <phcoder@gmail.com>
2021-10-04 04:40:14 +0200

    Fix currencyNameBySymbol SDK version check

--- src/jvmCommonMain/kotlin/au/id/micolous/metrodroid/util/Country.kt
+++ src/jvmCommonMain/kotlin/au/id/micolous/metrodroid/util/Country.kt
@@ -1,17 +1,7 @@
 @file:JvmName("CountryKtActual")
 package au.id.micolous.metrodroid.util
 
-import android.os.Build
 import java.util.*
-
-actual fun currencyNameBySymbol(symbol: String): String? =
-    Currency.getInstance(symbol)?.let {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            it.displayName
-        } else {
-            it.currencyCode
-        }
-    }
 
 private val specialLocales = mapOf(
     "CA" to Locale.CANADA,

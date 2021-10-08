a06c1ddf1965d1d9c031ddec0e34b6a019ea50e2
Vladimir Serbinenko <phcoder@vladimirs-macbook-air.home>
2021-10-08 09:59:43 +0200

    Fix interop signatures

--- native/metrodroid/metrodroid/HistoryViewController.swift
+++ native/metrodroid/metrodroid/HistoryViewController.swift
@@ -273,7 +273,7 @@ class HistoryViewController : UITableViewController, UISearchBarDelegate, UIDocu
         
         // Configure the cell’s contents.
         let el = effectiveHistory[indexPath.section].entries[indexPath.item]
-        let scanTime = TimestampKt_.date2Timestamp(date: el.date)
+        let scanTime = TimestampKt.date2Timestamp(date: el.date)
         cell.textLabel!.attributedText = Utils.localizeFormatted(RKt.R.string.scanned_at_format,
                                                                 TimestampFormatter.init().timeFormat(ts: scanTime),
                                                                 TimestampFormatter.init().dateFormat(ts: scanTime)).attributed

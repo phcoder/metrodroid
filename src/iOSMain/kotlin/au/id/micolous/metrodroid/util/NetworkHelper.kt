6f3eb8f277ad8a74a2a564c79f7f962caf2d9e7f
Vladimir Serbinenko <phcoder@gmail.com>
2021-10-15 21:52:37 +0200

    network helper attempt

--- src/iOSMain/kotlin/au/id/micolous/metrodroid/util/NetworkHelper.kt
+++ src/iOSMain/kotlin/au/id/micolous/metrodroid/util/NetworkHelper.kt
@@ -1,5 +1,7 @@
 package au.id.micolous.metrodroid.util
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.runBlocking
 import platform.Foundation.*
 
 actual fun sendPostRequest(urlString: String, request: ByteArray): ByteArray? {
@@ -7,17 +9,25 @@ actual fun sendPostRequest(urlString: String, request: ByteArray): ByteArray? {
         val url = NSURL(string = urlString)
         val conn = NSURLRequest(url=url)
         val session = NSURLSession.shared
+        conn.allowsConstrainedNetworkAccess = true
+        conn.allowsExpensiveNetworkAccess = true
         conn.httpMethod = "POST"
         conn.setValue(null, "Content-Type")
-
         conn.setValue("Metrodroid/" + Preferences.metrodroidVersion, "User-Agent")
         conn.httpBody = request
 
-
-        val recv = conn.inputStream
-        val recvBytes = recv.readBytes()
-        conn.disconnect()
-        return recvBytes
+        val channel = Channel<ByteArray?>()
+        return runBlocking {
+            val task = session.dataTaskWithURL(conn, completionHandler = { dat, response, error ->
+                val respTyped = response as NSHTTPURLResponse
+                if (error == null && respTyped.statusCode == 200)
+                    channel.send(dat.toByteArray())
+                else
+                    channel.send(null)
+            })
+            task.resume()
+            channel.receive()
+        }
     } catch (e: Exception) {
         return null
     }

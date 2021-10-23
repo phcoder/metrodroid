01840af61107ba1e3d9f20f7ccfe7a86726f597b
Vladimir Serbinenko <phcoder@gmail.com>
2021-10-23 02:15:58 +0200

    ListItem: split implementation from interface

--- src/commonMain/kotlin/au/id/micolous/metrodroid/transit/troika/TroikaBlock.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/troika/TroikaBlock.kt
@@ -7,6 +7,7 @@ import au.id.micolous.metrodroid.multi.R
 import au.id.micolous.metrodroid.time.*
 import au.id.micolous.metrodroid.transit.*
 import au.id.micolous.metrodroid.ui.ListItem
+import au.id.micolous.metrodroid.ui.ListItemInterface
 import au.id.micolous.metrodroid.util.NumberUtils
 import au.id.micolous.metrodroid.util.ImmutableByteArray
 import au.id.micolous.metrodroid.util.hexString
@@ -76,7 +77,7 @@ abstract class TroikaBlock private constructor(private val mSerial: Long,
         get() = TroikaSubscription(mExpiryDate, mValidityStart, mValidityEnd,
                 mRemainingTrips, mValidityLengthMinutes, mTicketType)
 
-    open val info: List<ListItem>?
+    open val info: List<ListItemInterface>?
         get() = null
 
     open val debug: List<ListItem>

abae2c0c558ebff81aae982702778edcc4bad7ed
Vladimir Serbinenko <phcoder@gmail.com>
2021-11-17 16:11:19 +0100

    iOS: Move XML parsing inside kotlin code, enable tests and fix old CEPAS parsing.
    The tests discovered that old CEPAS card breaks iOS parser. Fix it by doing the same prefiltering as on android

--- src/commonTest/kotlin/au/id/micolous/metrodroid/test/BaseInstrumentedTest.kt
+++ src/commonTest/kotlin/au/id/micolous/metrodroid/test/BaseInstrumentedTest.kt
@@ -1,13 +1,15 @@
 package au.id.micolous.metrodroid.test
 
+import au.id.micolous.metrodroid.card.Card
 import au.id.micolous.metrodroid.util.Input
 import kotlin.test.assertNotNull
 
 expect abstract class BaseInstrumentedTestPlatform() {
     fun setLocale(languageTag: String)
     fun loadAssetSafe(path: String) : Input?
-    fun listAsset(path: String) : List <String>?
 }
+
+expect fun loadCardXml(path: String): Card
 
 abstract class BaseInstrumentedTest : BaseInstrumentedTestPlatform() {
     fun loadSmallAssetBytesSafe(path: String): ByteArray? {
